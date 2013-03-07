ba979c2a5a0715ef4e58ba9b601a8020bcc6db3b
Dale Henrichs <dhenrich@vmware.com>
2013-03-07 12:17:55 -0800

    let's take her for a spin

---
+++ tests/testTravisCI.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+#
+#  Sample test driver that allows for customizing build/tests based on env vars defined in .travis.yml
+#
+#      -verbose flag causes unconditional transcript display
+#
+# Copyright (c) 2013 VMware, Inc. All Rights Reserved <dhenrich@vmware.com>.
+#
+
+
+if [ "$CONFIGURATIONx" = "x" ]; then
+  if [ "$BASELINEx" = "x" ]; then
+    echo "Must specify either BASELINE or CONFIGURATION"
+    exit 1
+  else
+    PROJECT_LINE="  baseline: '${BASELINE}';"
+    VERSION_LINE=""
+    REPOSITORY_LINE=""
+    FULL_CONFIG_NAME="BaselineOf${BASELINE}"
+  fi
+else
+  PROJECT_LINE="  configuration: '${CONFIGURATION}';"
+  VERSION_LINE="  version: '$VERSION';"
+  REPOSITORY_LINE="  repository: '$REPOSITORY';"
+  FULL_CONFIG_NAME="ConfigurationOf${BASELINE}"
+fi
+
+OUTPUT_PATH="${PROJECT_HOME}/tests/travisCI.st"
+
+cat - >> $OUTPUT_PATH << EOF
+ Transcript cr; show: 'travis--->${OUTPUT_pATH}'
+ "Load the configuration or baseline"
+ Metacello new
+ $PROJECT_LINE
+ $VERSION_LINE
+ $REPOSITORY_LINE
+   load: #( ${LOADS} ).
+  "Run the tests"
+  TravisCIHarness
+    value: #( '${FULL_CONFIG_NAME}' )
+    value: 'TravisCISuccess.txt' 
+    value: 'TravisCIFailure.txt'.
+EOF
+
+cat $OUTPUT_PATH
+
+$BUILDER_CI_HOME/testTravisCI.sh "$@"
+if [[ $? != 0 ]] ; then exit 1; fi
